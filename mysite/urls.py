8948fd97a9a4b8c032807dda9b6c39738bfc0253
Andrey Baldin <altkotm@gmail.com>
2018-09-25 16:39:16 +0700

    Issues #9 add function login\logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^blog/admin/', admin.site.urls),
+    url(r'^blog/accounts/', include('django.contrib.auth.urls')),
     url(r'', include('blog.urls')),
 ]
