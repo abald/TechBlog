3072a53e5915e44a5632b688e4cc8f59b49827f2
abald <a.baldin@nsu.ru>
2019-01-29 21:41:17 +0700

    new version blog

--- mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^blog/admin/', admin.site.urls),
-    url(r'^blog/accounts/', include('django.contrib.auth.urls')),
-    url(r'', include('blog.urls')),
-]
